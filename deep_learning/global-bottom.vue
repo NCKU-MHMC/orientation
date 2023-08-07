2d963a5a3ddfa429d870e4b4f59dfcf3a59c0e4c
toonnyy8 <toonnyy8@gmail.com>
2023-08-08 02:19:23 +0800

    DL1

---
+++ deep_learning/global-bottom.vue
@@ -0,0 +1,9 @@
+<!-- 一个显示页数的页脚示例 -->
+<template>
+  <footer
+    v-if="$slidev.nav.currentLayout !== 'cover'"
+    class="absolute bottom-2 right-2 p-2"
+  >
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+  </footer>
+</template>
